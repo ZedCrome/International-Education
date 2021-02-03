2ba0a95cb532a2ecd2f91d7d4b2ee6a9a1b161b2
MilanDB <65161992+MilanDB@users.noreply.github.com>
2021-02-03 14:31:19 +0100

    aoe damage

--- Toiletpaper and Zombies/Assets/Scripts/gun/PlungerBullet.cs
+++ Toiletpaper and Zombies/Assets/Scripts/gun/PlungerBullet.cs
@@ -5,13 +5,43 @@ using UnityEngine;
 public class PlungerBullet : MonoBehaviour, iPooled
 {
     public Rigidbody2D rb;
-    public float Force;
+    [Header("projectile Speed")]
+    public float m_force;
+
+    [Header("Explosive radius")]
+    public float m_radius;
+
+    [Header("Damage")]
+    public float m_damage;
+
 
     public void OnSpawn()
     {
-        rb.AddForce(transform.right * Force);
+        rb.AddForce(transform.right * m_force);
         Invoke("Destroy", 2f);
     }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag != "Player")
+        {
+            var hitColliders = Physics2D.OverlapCircleAll(transform.position, m_radius);
+
+            foreach (var hitCollider in hitColliders)
+            {
+                Debug.Log(m_damage);
+                var enemy = hitCollider.GetComponent<EnemyHealth>();
+                if (enemy)
+                {
+                    enemy.ChangeHealth(m_damage);
+                    Debug.Log(m_damage);
+                }
+
+            }
+            gameObject.SetActive(false);
+        }
+    }
+
     private void Destroy()
     {
         gameObject.SetActive(false);
