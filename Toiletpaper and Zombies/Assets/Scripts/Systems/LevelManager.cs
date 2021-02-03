f8f60bf63738636196c03376258b6125512952de
ZedCrome <robin.lindevy@live.com>
2021-02-03 13:03:28 +0100

    Added some HUD-options

---
+++ Toiletpaper and Zombies/Assets/Scripts/Systems/LevelManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
