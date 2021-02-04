f0e66bf8f03ec30faf4faedb386a16ed325e6565
MilanDB <65161992+MilanDB@users.noreply.github.com>
2021-02-04 23:13:43 +0100

    new Gun kind of

---
+++ Toiletpaper and Zombies/Assets/Scripts/gun/Shooting.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject FirePoint;
+    private bool m_canShoot = true;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && m_canShoot == true)
+        {
+            Shoot();
+            m_canShoot = false;
+            Invoke("canShoot", 0.5f);
+        }
+
+    }
+
+    private void canShoot()
+    {
+        m_canShoot = true;
+    }
+
+    private void Shoot()
+    {
+        GameObject m_plungerAmmo = ObjectPool.instance.SpawnPool("StickerAmmo", FirePoint.transform.position, FirePoint.transform.rotation);
+        if (m_plungerAmmo != null)
+        {
+            m_plungerAmmo.SetActive(true);
+        }
+
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+    }
+}
