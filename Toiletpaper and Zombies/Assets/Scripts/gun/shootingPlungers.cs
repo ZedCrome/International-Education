a66db135f271aa3d42f585acca3a81d97097fe13
MilanDB <65161992+MilanDB@users.noreply.github.com>
2021-02-04 22:36:29 +0100

    shooting coooldown

--- Toiletpaper and Zombies/Assets/Scripts/gun/shootingPlungers.cs
+++ Toiletpaper and Zombies/Assets/Scripts/gun/shootingPlungers.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class shootingPlungers : MonoBehaviour
 {
     public GameObject FirePoint;
-   
+    private bool m_canShoot = true;
+    
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && m_canShoot == true)
         {
             Shoot();
+            m_canShoot = false;
+            Invoke("canShoot", 0.5f);
         }
             
+    }
+
+    private void canShoot()
+    {
+        m_canShoot = true;
     }
 
     private void Shoot()
